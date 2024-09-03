e9e6267d5b389d2a75250b62e56b5e9986b379b9
Edrei Cruz <edrei.cruz@cerocatorce.com>
2024-09-03 16:32:50 -0500

    Feature: Change answers importer.

--- app/Imports/AnswersImport.php
+++ app/Imports/AnswersImport.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Monolog\Handler\IFTTTHandler;
 
 class AnswersImport implements ToCollection
 {
@@ -20,43 +21,75 @@ class AnswersImport implements ToCollection
     {
         foreach ($rows as $row)
         {
-            if($row[1] != null){
-                $val1 = false;
-                if(1 == $row[5])$val1=true;
-                QuestionAnswer::create([
-                    'name' => $row[1],
-                    'answer' => $row[1],
-                    'slug' => Str::slug(implode(' ', array_slice(explode(' ', $row[1]), 0, 10)).$row[7], '-'),
-                    'validity' => $val1,
-                    'question_id' => $row[7],
-                ]);
-                $val2 = false;
-                if(2 == $row[5])$val2=true;
-                QuestionAnswer::create([
-                    'name' => $row[2],
-                    'answer' => $row[2],
-                    'slug' => Str::slug(implode(' ', array_slice(explode(' ', $row[2]), 0, 10)).$row[7], '-'),
-                    'validity' => $val2,
-                    'question_id' => $row[7],
-                ]);
-                $val3 = false;
-                if(3 == $row[5])$val3=true;
-                QuestionAnswer::create([
-                    'name' => $row[3],
-                    'answer' => $row[3],
-                    'slug' => Str::slug(implode(' ', array_slice(explode(' ', $row[3]), 0, 10)).$row[7], '-'),
-                    'validity' => $val3,
-                    'question_id' => $row[7],
-                ]);
-                $val4 = false;
-                if(4 == $row[5])$val4=true;
-                QuestionAnswer::create([
-                    'name' => $row[4],
-                    'answer' => $row[4],
-                    'slug' => Str::slug(implode(' ', array_slice(explode(' ', $row[4]), 0, 10)).$row[7], '-'),
-                    'validity' => $val4,
-                    'question_id' => $row[7],
-                ]);
+            if ($row[0] == 'ANSWER 1') {
+                continue;
+            }
+
+            if($row[5] != null){
+                $types = [
+                    'penal' => '1',
+                    'civil' => '2',
+                    'extincion' => '3'
+                ];
+
+                $question = Question::where('name', $row[5])->first();
+                if (!$question) {
+                    $question = Question::create([
+                        'name' => $row[5],
+                        'question' => $row[5],
+                        'slug' => Str::slug(implode(' ', array_slice(explode(' ', $row[5]), 0, 10)), '-'),
+                        'type' => $types[$row[6]]
+                    ]);
+                }
+
+
+                if ($row[0] != null) {
+                    $val1 = false;
+                    if($row[4] == 1)$val1=true;
+                    QuestionAnswer::create([
+                        'name' => $row[0],
+                        'answer' => $row[0],
+                        'slug' => Str::slug(implode(' ', array_slice(explode(' ', $row[0]), 0, 10)).$question->id, '-'),
+                        'validity' => $val1,
+                        'question_id' => $question->id,
+                    ]);
+                }
+
+                if ($row[1] != null) {
+                    $val2 = false;
+                    if($row[4] == 2)$val2=true;
+                    QuestionAnswer::create([
+                        'name' => $row[1],
+                        'answer' => $row[1],
+                        'slug' => Str::slug(implode(' ', array_slice(explode(' ', $row[1]), 0, 10)).$question->id, '-'),
+                        'validity' => $val2,
+                        'question_id' => $question->id,
+                    ]);
+                }
+
+                if ($row[2] != null) {
+                    $val3 = false;
+                    if($row[4] == 3)$val3=true;
+                    QuestionAnswer::create([
+                        'name' => $row[2],
+                        'answer' => $row[2],
+                        'slug' => Str::slug(implode(' ', array_slice(explode(' ', $row[2]), 0, 10)).$question->id, '-'),
+                        'validity' => $val3,
+                        'question_id' => $question->id,
+                    ]);
+                }
+
+                if ($row[3] != null) {
+                    $val4 = false;
+                    if($row[4] == 4)$val4=true;
+                    QuestionAnswer::create([
+                        'name' => $row[3],
+                        'answer' => $row[3],
+                        'slug' => Str::slug(implode(' ', array_slice(explode(' ', $row[3]), 0, 10)).$question->id, '-'),
+                        'validity' => $val4,
+                        'question_id' => $question->id,
+                    ]);
+                }
             }
         }
     }
